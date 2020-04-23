b0c2c52ef2eba6a4500aa3d1696dc2d455a4b0e7
philani26 <gumedephilani0@gmail.com>
2020-04-23 08:04:30 -1000

    api

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () =>{
         useFindAndModify: false,
         useUnifiedTopology: true 
     })
-    console.log(`MangoDB connected ${conn.connection.host}`);
+    console.log(`MangoDB connected ${conn.connection.host}`.cyan.underline.bold);
 }
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const dotenv = require ('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db')
 
 
@@ -26,10 +27,10 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/bootcamp', bootcamps)
 
 const PORT = process.env.PORT || 3000 ;
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow))
 
 process.on('unhandledRejection', (err, promise) =>{
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     server.close(()=> process.exit(1))
 })
   
